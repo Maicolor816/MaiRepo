c101d755ade9506f6b811b657f7f6ee8a598ddb4
mai <mai.btrax@gmail.com>
2013-01-01 23:58:02 +0900

    Java documents

---
+++ java/Manager.java
@@ -0,0 +1,7 @@
+public class Manager{
+	public static void main(String[] args){
+		Ningen hito = new Ningen("mai", 157);
+		hito.taberu();
+		hito.neru();
+	}
+} 	
---
+++ java/Ningen.java
@@ -0,0 +1,17 @@
+public class Ningen{
+	private String name;
+	private int shincho;
+	
+	public Ningen(String name, int shincho){
+		this.name = name;
+		this.shincho = shincho;
+	}
+	
+	public void taberu(){
+		System.out.println("?????");
+	}
+	
+	public void neru(){
+		System.out.println("????");
+	}
+}
