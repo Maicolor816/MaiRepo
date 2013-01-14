1249d837ae3462ca5f7ea78da65ec3a1eafbacdd
mai <mai.btrax@gmail.com>
2013-01-15 06:09:25 +0900

    Plus Vim tutorials

---
+++ vim/test.js
@@ -0,0 +1,28 @@
+v visual mode -> select lines 
+i insert mode 
+
+//移動系のコマンド
+gg:先頭に行く
+G:最後に行く
+Ctrl+b
+Ctrl+f:次のページ
+
+cut:x,dd(line) dd5(5 lines)
+copy:yy, 3yy
+paste:p
+
+検索
+:s/word
+n: 下方向に検索
+N;上方向に検索
+*,#:今カーソルがある位置の単語を探す
+
+置換
+maimai
+
+mai
+
+
+session_start
+
+
